1cea26f4a63a8475bd342fd2581e36e891bdaa3a
NealRame <julien@graziano.fr>
2013-08-12 13:21:23 +0200

    Remove remaining Debug directory from sources

--- sources/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lnrgraph -lnraudio -lboost_regex -lboost_filesystem -lboost_system
-
