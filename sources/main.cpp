90fe9aa6645a1032a2c972be7c013e713a2386a4
NealRame <julien@graziano.fr>
2013-08-14 17:04:21 +0200

    Pulled down update to module/nr-audio-lib

--- sources/main.cpp
+++ sources/main.cpp
@@ -10,13 +10,11 @@
 #include <iostream>
 #include <string>
 
+#include <boost/any.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/format.hpp>
 #include <boost/filesystem.hpp>
-#include <boost/program_options/option.hpp>
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/variables_map.hpp>
-#include <boost/program_options/parsers.hpp>
+#include <boost/program_options.hpp>
 
 #include <AudioBuffer.h>
 #include <AudioError.h>
@@ -25,38 +23,116 @@
 #include <codec/AudioPCMCoder.h>
 
 #include <GraphBrush.h>
+#include <GraphColor.h>
 #include <GraphError.h>
+#include <GraphSize.h>
 #include <GraphSurface.h>
 
 #include "Parser.h"
 #include "WaveFormGenerator.h"
 
+#define VERSION_STRING "development"
+
+namespace com {
+namespace nealrame {
+namespace graph {
+void validate(boost::any &v, const std::vector<std::string> &values, Brush *, int) {
+	using namespace boost::program_options;
+	validators::check_first_occurrence(v);
+	validators::get_single_string(values);
+	// const std::string &s = po::validators::get_single_string(values);
+	v = boost::any(Brush(Color::Black));
+}
+
+void validate(boost::any &v, const std::vector<std::string> &values, Size *, int) {
+	namespace po = boost::program_options;
+	po::validators::check_first_occurrence(v);
+	po::validators::get_single_string(values);
+	// const std::string &s = po::validators::get_single_string(values);
+	v = boost::any(Size(1024, 256));
+} } } }
+
 using namespace com::nealrame;
+
+const graph::Brush DefaultBackground = graph::Brush(graph::Color::Black);
+const graph::Brush DefaultForeground = graph::Brush(graph::Color::White);
+const graph::Size  DefaultSize       = graph::Size(1024, 256);
 
 namespace po = boost::program_options;
 
 int main(int argc, char **argv) {
 	std::string input, output;
 
-	po::options_description options("Allowed options");
-	options.add_options()
-		("help,h", "print this help and exit")
-		("output,o", po::value<std::string>(&output), "set output file")
-		("input,i", po::value<std::string>(&input), "set input file");
+	po::options_description common_options("Common options");
+	common_options.add_options()
+		(
+			"help,h", 
+			"print this help and exit"
+		) (
+			"output,o", 
+			po::value<std::string>(&output), 
+			"set output file"
+		) (
+			"version,v", 
+			"print version string and exit"
+		);
+
+	po::options_description render_options("Render options");
+	render_options.add_options()
+		(
+			"background,b", 
+			po::value<graph::Brush>()->default_value(DefaultBackground, "#000"),
+			"set the background brush"
+		) (
+			"foreground,f", 
+			po::value<graph::Brush>()->default_value(DefaultForeground, "#fff"),
+			"set the foreground brush"
+		) (
+			"size,s",
+			po::value<graph::Size>()->default_value(DefaultSize, "1024x256"),
+			"set the waveform bounding box"
+		);
+
+	po::options_description hidden_options("Hidden options");
+	hidden_options.add_options()
+		(
+			"input,i",
+			po::value<std::string>(&input), 
+			"set input file"
+		);
+
+	po::options_description cmdline_options;
+	cmdline_options
+		.add(common_options)
+		.add(render_options)
+		.add(hidden_options);
+
+	po::options_description visible_options;
+	visible_options
+		.add(common_options)
+		.add(render_options);
 
 	po::positional_options_description args;
 	args.add("input", 1);
 
 	po::variables_map vm;
 
-	po::store(po::command_line_parser(argc, argv).options(options).positional(args).run(), vm);
+	po::store(po::command_line_parser(argc, argv)
+			.options(cmdline_options)
+			.positional(args).run(), vm);
 	po::notify(vm);
 
 	if (vm.count("help")) {
 		std::cout << (boost::format("Usage: %1% [options] file.[mp3|ogg|wav]")
 				% boost::filesystem::basename(argv[0])).str() << std::endl;
-		std::cout << options << std::endl;
-		return 1;
+		std::cout << visible_options << std::endl;
+		return 0;
+	}
+
+	if (vm.count("version")) {
+		std::cout << (boost::format("Version: %1%") 
+				% VERSION_STRING).str() << std::endl;
+		return 0;
 	}
 
 	if (! vm.count("input")) {
